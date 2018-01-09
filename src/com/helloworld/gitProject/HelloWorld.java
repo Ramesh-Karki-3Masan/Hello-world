a9bfd73801c3ba2520e9c631c5ba08445fe09edd
Ramesh-Karki-3Masan <karkiramesh.st@gmail.com>
2018-01-08 16:29:00 -0800

    Initial change

---
+++ src/com/helloworld/gitProject/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.helloworld.gitProject;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		out.print("Hello-World from Github practice project");
+	}
+
+}
